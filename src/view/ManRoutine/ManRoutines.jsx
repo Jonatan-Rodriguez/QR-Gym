5a85d70f36f7948e90cbad2d764265e396dbde02
Jona <jonatarodriguez1998@gmail.com>
2024-09-18 00:57:55 -0300

    implementando tailwind

--- src/view/ManRoutine/ManRoutines.jsx
+++ src/view/ManRoutine/ManRoutines.jsx
@@ -1,10 +1,11 @@
 //styled
 import { ContainerManRoutines } from './manRoutines.styled';
+import './manRutines.css';
 
 function ManRutines() {
     return(
         <ContainerManRoutines>
-            <h1>Rutinas de hombre</h1>
+            <h1 className='font-bold underline'>Rutinas de hombre</h1>
         </ContainerManRoutines>
     )
 }
