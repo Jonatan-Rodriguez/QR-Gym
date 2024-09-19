63e42639d9a9a062b9a61425110b8a4cdf4e2b50
Jona <jonatarodriguez1998@gmail.com>
2024-09-19 17:56:49 -0300

    terminando home

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,20 +1,47 @@
-//styled
-import { ContainerHome } from './home.styled';
+import { ContainerHome, Title } from './home.styled';
+import menTraining from '../../assets/img/menTraining.jpg';
+import womenTraining from '../../assets/img/womenTraining.jpg';
+import gymBackground from '../../assets/img/gym-background.jpg'; // Imagen de fondo
 
 import { Link } from 'react-router-dom';
 
 function Home() {
-    return(
-        <ContainerHome>
-            <h1>home</h1>
+    return (
+        <ContainerHome style={{ backgroundImage: `url(${gymBackground})` }}>
+            {/* Título principal */}
+            <Title>
+                <span className="text-orange-500">R</span>UTINAS
+            </Title>
+
+            {/* Card para Hombre */}
             <Link to='/manRutines'>
-                <button>Hombre</button>
+                <div style={{ backgroundImage: `url(${menTraining})` }} className='flex flex-col items-center justify-end bg-cover bg-center bg-no-repeat w-96 h-44 rounded-3xl hover:scale-110 transition-transform duration-300 ease-in-out'>
+                    <div className='gradient flex items-center justify-center h-2/4 w-full rounded-b-3xl'>
+                        <div className='flex items-center justify-between w-full px-4'>
+                            <p className='text-3xl font-title uppercase'>
+                                <span className="text-orange-500">H</span>ombre
+                            </p>
+                            <svg className='text-3xl' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="white"><path d="m12.052 14.829l1.414 1.414L17.71 12l-4.243-4.243l-1.414 1.415L13.88 11H6.343v2h7.537z"/><path fill-rule="evenodd" d="M19.778 19.778c4.296-4.296 4.296-11.26 0-15.556s-11.26-4.296-15.556 0s-4.296 11.26 0 15.556s11.26 4.296 15.556 0m-1.414-1.414A9 9 0 1 0 5.636 5.636a9 9 0 0 0 12.728 12.728" clip-rule="evenodd"/></g></svg>
+                        </div>
+                    </div>
+                </div>
             </Link>
+
+            {/* Card para Mujer */}
             <Link to='/womenRutines'>
-                <button>Mujer</button>
+                <div style={{ backgroundImage: `url(${womenTraining})` }} className='flex flex-col items-center justify-end bg-cover bg-center bg-no-repeat w-96 h-44 rounded-3xl hover:scale-110 transition-transform duration-300 ease-in-out'>
+                    <div className='gradient flex items-center justify-center h-2/4 w-full rounded-b-3xl'>
+                        <div className='flex items-center justify-between w-full px-4'>
+                            <p className='text-3xl font-title uppercase'>
+                                <span className="text-orange-500">M</span>ujer
+                            </p>
+                            <svg className='text-3xl' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="white"><path d="m12.052 14.829l1.414 1.414L17.71 12l-4.243-4.243l-1.414 1.415L13.88 11H6.343v2h7.537z"/><path fill-rule="evenodd" d="M19.778 19.778c4.296-4.296 4.296-11.26 0-15.556s-11.26-4.296-15.556 0s-4.296 11.26 0 15.556s11.26 4.296 15.556 0m-1.414-1.414A9 9 0 1 0 5.636 5.636a9 9 0 0 0 12.728 12.728" clip-rule="evenodd"/></g></svg>
+                        </div>
+                    </div>
+                </div>
             </Link>
         </ContainerHome>
-    )
+    );
 }
 
 export default Home;
--- src/view/Home/home.styled.js
+++ src/view/Home/home.styled.js
@@ -1,5 +1,64 @@
 import styled from 'styled-components';
 
 export const ContainerHome = styled.div`
-    background-color: burlywood;
-`
+    width: 100vw;
+    height: 100vh;
+    padding: 3% 5%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    row-gap: 10px;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed; /* Fija la imagen al fondo */
+    position: relative;
+
+    /* Imagen de fondo y oscurecimiento */
+    &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.7); /* Oscurecimiento */
+        z-index: 1; /* Debe estar detrás del contenido */
+    }
+
+    /* Ajustar el contenido del contenedor */
+    & > * {
+        position: relative;
+        z-index: 2; /* El contenido está por encima del fondo oscuro */
+    }
+
+    .gradient {
+        background: rgb(42, 42, 42);
+        background: linear-gradient(0deg, rgba(42, 42, 42, 0.80) 70%, rgba(255, 255, 255, 0) 100%);
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+    .font-title {
+        font-family: 'Bebas Neue', sans-serif;
+        color: white;
+    }
+
+    span {
+        color: #f97316; /* Naranja */
+    }
+`;
+
+export const Title = styled.h1`
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 6rem;
+    text-transform: uppercase;
+    letter-spacing: 0.15rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+
+    span {
+        color: #f97316; /* Naranja */
+    }
+`;
